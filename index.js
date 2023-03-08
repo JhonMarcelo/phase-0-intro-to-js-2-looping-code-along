935a940224eb031596421368c72dd1b15ad5ea02
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-03-08 09:26:47 -0500

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+
+function writeCards(name, ocation){
+    const messages = [];
+for (let i = 0; i < name.length; i++){
+    
+   messages[i]= `Thank you, ${name[i]}, for the wonderful ${ocation} gift!`;
+
+}
+return messages;
+    
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
+
+
+
+
+function countDown(number){
+
+    let i = 0;
+    while(i < number+1){
+        console.log(number - i);
+        i++;
+    }
+}
